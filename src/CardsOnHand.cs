5f76298b98fcdf684177fac083fef25a3fe47c1d
kizdebski <kizdebski@infusion.com>
2015-07-11 15:38:41 +0200

    check cards

--- src/CardsOnHand.cs
+++ src/CardsOnHand.cs
@@ -1,16 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Dynamic;
 using System.Dynamic;
+using System.Linq;
 using Newtonsoft.Json.Linq;
 
 namespace Nancy.Simple
 {
     public static partial class PokerPlayer
     {
-        public static Hand CheckCardsOnHand(JObject gameState)
+        public static Hand CheckCardsOnHand(RequestStructure.GameState gameState)
         {
             try
             {
+                var cards = gameState.OurCards.Concat(gameState.CommunityCards).ToList();
+                if (IsPair(cards))
+                {
+                    return Hand.Pair;
+                }
                 return Hand.Nothing;
             }
             catch (Exception e)
@@ -18,6 +25,11 @@ namespace Nancy.Simple
                 Console.WriteLine("Error occured: " + e.Message + "\n\t" + e.StackTrace);
                 return Hand.Nothing;
             }
+        }
+
+        public static bool  IsPair(List<RequestStructure.Card> cards)
+        {
+            return false;
         }
     }
 
