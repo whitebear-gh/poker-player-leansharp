d6dbcb7afbd571651fd4dc5a1461089be8549869
Piotr Martyński <p.martynski@gmail.com>
2015-07-11 10:43:02 +0200

    Basic bet strategy

--- src/CalculateBet.cs
+++ src/CalculateBet.cs
@@ -1,12 +1,19 @@
-﻿namespace Nancy.Simple
+﻿using System;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace Nancy.Simple
 {
     public static partial class PokerPlayer
     {
 
 
-        public static dynamic CalculateBet()
+        public static dynamic CalculateBet(JObject gameState, dynamic rank)
         {
-            return null;
-        } 
+            int currentPot = (int)gameState["pot"];
+            var maxBet = ((JArray) gameState["players"]).Max(p => (int) p["bet"]);
+
+            return Math.Max(maxBet, currentPot);
+        }
     }
 }
