ce3377b4073728963342d6b0616cf127ebf1d875
Lollipop <lollipop@lollipop.lollipop>
2015-07-11 13:57:23 +0200

    Ranks

--- src/CardsOnHand.cs
+++ src/CardsOnHand.cs
@@ -11,30 +11,51 @@ namespace Nancy.Simple
 {
     public static partial class PokerPlayer
     {
+        private static Dictionary<string, int> cardsPower = new Dictionary<string, int>();
+
         public static dynamic CheckCardsOnHand(RequestStructure.GameState gameState)
         {
             try
             {
-                var playerID = gameState.InAction;
-                var cards = gameState.Players[int.Parse(playerID)].HoldCards;
-                var communityCards = gameState.CommunityCards;
-                var cardsOverall = cards.Concat(communityCards);
+                var cardsOverall = gameState.CommunityCards.Concat(gameState.OurPlayer.HoldCards).ToList();
 
-                checkIfPair(cardsOverall);
+                switch (gameState.CommunityCards.Count)
+                {
+                    case 3:
+                        checkPairs(3, cardsOverall);
+                        break;
 
+                    case 5:
+                        checkPairs(5, cardsOverall);
+                        break;
+
+                    default:
+                        return Hand.Nothing;
+                }
             }
             catch (Exception e)
             {
                 Console.WriteLine("Error occured: " + e.Message + "\n\t" + e.StackTrace);
-                return Hand.Nothing;
             }
 
             return Hand.Nothing;
         }
 
-        private static void checkIfPair(IEnumerable<RequestStructure.Card> cardsOverall)
+        private static void checkPairs(int cardsCount, List<RequestStructure.Card> cardsOverall)
         {
-            throw new NotImplementedException();
+            var currentCardRank = cardsOverall[0].Rank;
+
+            if (cardsCount > 0)
+            {
+                if (cardsPower.ContainsKey(currentCardRank))
+                {
+                    cardsPower[currentCardRank]++;
+                }
+                else
+                {
+                    cardsPower.Add(currentCardRank, Rank.GetCardValue(currentCardRank));
+                }
+            }
         }
     }
 }
