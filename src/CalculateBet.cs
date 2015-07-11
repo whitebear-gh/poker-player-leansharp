b0780b50e0b1b27ea385baebcfb798e662aa8add
Piotr Martyński <p.martynski@gmail.com>
2015-07-11 11:34:53 +0200

    Top bet player based strategy

--- src/CalculateBet.cs
+++ src/CalculateBet.cs
@@ -11,15 +11,27 @@ namespace Nancy.Simple
         public static dynamic CalculateBet(JObject gameState, dynamic rank)
         {
             var currentPot = GetCurrentPot(gameState);
-            var maxBet = GetMaxBet(gameState);
+            var maxBetPlayer = GetMaxBetPlayer(gameState);
             var round = GetRound(gameState);
 
-            var actualBet = Math.Max(maxBet, currentPot);
-            if (round > 10)
+            var myStack = GetMyStack(gameState);
+
+            var actualBet = Math.Max(maxBetPlayer.Bet, currentPot);
+            if (round > 20)
             {
                 actualBet = actualBet*round/100;
             }
+            else if (maxBetPlayer.Bet > myStack)
+            {
+                actualBet = 0;
+            }
+
             return actualBet;
+        }
+
+        private static int GetMyStack(JObject gameState)
+        {
+            return ((int) gameState["players"].Single(p => (string) p["name"] == "LeanSharp")["stack"]);
         }
 
         private static int GetRound(JObject gameState)
@@ -33,10 +45,18 @@ namespace Nancy.Simple
             return currentPot;
         }
 
-        private static int GetMaxBet(JObject gameState)
+        private static dynamic GetMaxBetPlayer(JObject gameState)
         {
-            var maxBet = ((JArray) gameState["players"]).Max(p => (int) p["bet"]);
-            return maxBet;
+            var players = gameState["players"].Select(p =>
+            new
+            {
+                Bet = p["bet"],
+                Stack = p["stack"]
+            });
+            var topPlayer = players.OrderBy(p => p.Bet).Last();
+            return topPlayer;
         }
+
+
     }
 }
