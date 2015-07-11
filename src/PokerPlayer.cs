6622dd6288b8f4bc43e5832c7b769365c7d79a64
Wojciech Izykowski <wizykowski@gmail.com>
2015-07-11 10:34:49 +0200

    try-catch

--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using System;
+using Newtonsoft.Json.Linq;
 
 namespace Nancy.Simple
 {
@@ -11,10 +12,27 @@ namespace Nancy.Simple
 	    }
 		public static readonly string VERSION = "Default C# folding player";
 
+
+
+        /// <summary>
+        /// Use this method to return the value You want to bet
+        /// </summary>
+        /// <param name="gameState"></param>
+        /// <returns></returns>
 		public static int BetRequest(JObject gameState)
 		{
-			//TODO: Use this method to return the value You want to bet
-			return 1000;
+            try
+            {
+                int bet = CalculateBet();
+
+                return bet;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error occured: " + e.Message + "\n\t" + e.StackTrace);
+                
+                return 1000;
+            }
 		}
 
 		public static void ShowDown(JObject gameState)
