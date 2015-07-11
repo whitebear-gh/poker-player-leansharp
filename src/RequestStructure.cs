25240db4c5560dc443ae6868ff5c403a4fbc70b2
kizdebski <kizdebski@infusion.com>
2015-07-11 12:08:54 +0200

    Object Mappings works +tests

--- src/RequestStructure.cs
+++ src/RequestStructure.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -50,29 +51,31 @@ namespace Nancy.Simple
                 Players = JsonConvert.DeserializeObject<List<Player>>(gameState["players"].ToString());
                 TournamentId = gameState["tournament_id"].ToString();
                 GameId = gameState["game_id"].ToString();
-                Round = gameState["round"].ToString();
-                BetIndex = gameState["bet_index"].ToString();
-                SmallBlind = gameState["small_blind"].ToString();
-                Orbits = gameState["orbits"].ToString();
+                Round = Convert.ToInt32(gameState["round"].ToString());
+                BetIndex = Convert.ToInt32(gameState["bet_index"].ToString());
+                SmallBlind = Convert.ToDouble(gameState["small_blind"].ToString());
+                Orbits = Convert.ToInt32(gameState["orbits"].ToString());
                 Dealer = gameState["dealer"].ToString();
                 CommunityCards = JsonConvert.DeserializeObject<List<Card>>(gameState["community_cards"].ToString());
-                CurrentBuyIn = gameState["current_buy_in"].ToString();
-                Pot = gameState["pot"].ToString();
-                Pot = gameState["in_action"].ToString();
+                CurrentBuyIn = Convert.ToDouble(gameState["current_buy_in"].ToString());
+                Pot = Convert.ToDouble(gameState["pot"].ToString());
+                InAction = Convert.ToInt32(gameState["in_action"].ToString()); 
+                MinimumRaise = Convert.ToDouble(gameState["minimum_raise"].ToString());
             }
             public List<Player> Players { get; set; }
 
             public string TournamentId { get; set; }
             public string GameId { get; set; }
-            public string Round { get; set; }
-            public string BetIndex { get; set; }
-            public string SmallBlind { get; set; }
-            public string Orbits { get; set; }
+            public int Round { get; set; }
+            public int BetIndex { get; set; }
+            public double SmallBlind { get; set; }
+            public int Orbits { get; set; }
             public string Dealer { get; set; }
             public List<Card> CommunityCards { get; set; }
-            public string CurrentBuyIn { get; set; }
-            public string Pot { get; set; }
-            public string InAction { get; set; }
+            public double CurrentBuyIn { get; set; }
+            public double MinimumRaise { get; set; }
+            public double Pot { get; set; }
+            public int InAction { get; set; } 
             
   //          "tournament_id":"550d1d68cd7bd10003000003",
   //"game_id":"550da1cb2d909006e90004b1",
